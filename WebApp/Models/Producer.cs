0314ae0c95951709f54b5ab88f5721e317e1bb07
cheez <cheezballz03@gmail.com>
2023-04-14 15:08:06 +0300

    +Producer
    added Producer class in Models folder

---
+++ WebApp/Models/Producer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models;
+
+public class Producer
+{
+    [Key]
+    public int Id { get; set; }
+    public string PfpUrl { get; set; }
+    public string FullName { get; set; }
+    public string Bio { get; set; }
+}
