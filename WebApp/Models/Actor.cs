c9d4ebf860932ba8978399299c0c22bcc085a52d
cheez <cheezballz03@gmail.com>
2023-04-14 14:54:48 +0300

    +Actor
    added class Actor to Models folder

---
+++ WebApp/Models/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string PfpUrl { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
